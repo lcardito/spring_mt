4e777b8f34d902cf80dbb23c6e85777917e8afaf
Luigi Cardito <lcardito@clearvision-cm.com>
2016-08-15 15:59:48 +0100

    small cleanup

--- src/test/java/com/clearvision/spectrum/dao/tenant/ApplicationDaoTest.java
+++ src/test/java/com/clearvision/spectrum/dao/tenant/ApplicationDaoTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
@@ -15,7 +15,7 @@ import java.util.Optional;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest(classes = {SpectrumTest.class})
 @Transactional("tenantTransactionManager")
 public class ApplicationDaoTest {
